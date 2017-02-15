1f79a863ede6642690233d1097265ba939c4dd86
Theo Steininger <theo.steininger@ultimanet.de>
2017-02-15 23:46:58 +0100

    Added observable mixins to __init__ file.

--- imagine/observers/hammurapy/observable_mixins/__init__.py
+++ imagine/observers/hammurapy/observable_mixins/__init__.py
@@ -1,3 +1,8 @@
 # -*- coding: utf-8 -*-
 
+from dm_mixin import DMMixin
+from dust_mixin import DustMixin
+from ff_mixin import FFMixin
 from rm_mixin import RMMixin
+from sync_mixin import SyncMixin
+from tau_mixin import TauMixin
