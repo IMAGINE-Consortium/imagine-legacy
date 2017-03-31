cbbe1fbb6ddb5b477eef6d8965dc319db3fd0ef6
Theo Steininger <theo.steininger@ultimanet.de>
2017-03-31 11:19:10 +0200

    Added repository methods to Observable class.

--- imagine/observables/observable.py
+++ imagine/observables/observable.py
@@ -25,3 +25,23 @@ class Observable(Field):
             self._ensemble_mean = self.mean(spaces=0)
         finally:
             return self._ensemble_mean
+
+    def _to_hdf5(self, hdf5_group):
+        if self._ensemble_mean is not None:
+            return_dict = {'ensemble_mean': self._ensemble_mean}
+        else:
+            return_dict = {}
+        return_dict.update(
+                   super(Observable, self)._to_hdf5(hdf5_group=hdf5_group))
+        return return_dict
+
+    @classmethod
+    def _from_hdf5(cls, hdf5_group, repository):
+        new_field = super(Observable, cls)._from_hdf5(hdf5_group=hdf5_group,
+                                                      repository=repository)
+        try:
+            observable_mean = repository.get('ensemble_mean', hdf5_group)
+            new_field._observable_mean = observable_mean
+        except(KeyError):
+            pass
+        return new_field
