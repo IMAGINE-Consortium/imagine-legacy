1a2cb14e8cf3ab22c75e47e42a6ea39446284c73
Theo Steininger <theo.steininger@ipt.ai>
2017-09-28 11:48:42 +0200

    Fixed bug in WMAP3yrMixin

--- imagine/observers/hammurapy/model_mixins/wmap3yr_mixin.py
+++ imagine/observers/hammurapy/model_mixins/wmap3yr_mixin.py
@@ -5,7 +5,9 @@ from imagine.magnetic_fields.wmap3yr_magnetic_field import WMAP3yrMagneticField
 
 class WMAP3yrMixin(object):
     __parameter_dict = {('./Galaxy/MagneticField/Regular', 'type'): 'WMAP',
-                        ('./Galaxy/MagneticField/Random', 'cue'): '1'}
+                        ('./Galaxy/MagneticField/Random', 'cue'): '1',
+                        ('./Galaxy/MagneticField/Random', 'type'): 'Anisoglob',
+                        }
 
     @property
     def magnetic_field_class(self):
