8276ccc3a73684e0ab5cb2635e2a5f3b668d1d00
Theo Steininger <theo.steininger@ultimanet.de>
2017-11-10 02:52:49 +0100

    bugfix

--- imagine/likelihoods/ensemble_likelihood/ensemble_likelihood.py
+++ imagine/likelihoods/ensemble_likelihood/ensemble_likelihood.py
@@ -25,10 +25,10 @@ class EnsembleLikelihood(Likelihood):
         # if the first element in the domain tuple is a FieldArray we must
         # extract the data
         if isinstance(data.domain[0], FieldArray):
-            stripped_data = Field(domain=data.domain[1:],
-                                  val=data.val.get_full_data()[0],
-                                  distribution_strategy='not')
-        return stripped_data
+            data = Field(domain=data.domain[1:],
+                         val=data.val.get_full_data()[0],
+                         distribution_strategy='not')
+        return data
 
     def __call__(self, observable):
         field = observable[self.observable_name]
